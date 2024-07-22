b5767d1fcd9f25c6169fe6114771f28980405559
Jose Espejo <joseespejo@yahoo.com>
2024-07-22 12:25:51 +0100

    Initial Commit

---
+++ EmojiArt/EmojiArtApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmojiArtApp.swift
+//  EmojiArt
+//
+//  Created by Jose Espejo on 22/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct EmojiArtApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
